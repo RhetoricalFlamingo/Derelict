b6534d0800ab629ee46ee5a8f233bad937c51bbf
MC <mcmailbox014@gmail.com>
2018-10-02 16:37:12 -0400

    Faster, snappier player controller

--- Assets/Scripts/standardBullet.cs
+++ Assets/Scripts/standardBullet.cs
@@ -1,8 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class standardBullet : MonoBehaviour
 {
 	public int damage = 0;
+
+	private void FixedUpdate()
+	{
+		this.GetComponent<Rigidbody2D>().velocity *= 1.04f;
+	}
 }

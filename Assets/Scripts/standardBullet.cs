f827d2da78252a3031f37e7237a2fb099bb3f534
MC <mcmailbox014@gmail.com>
2018-10-04 15:09:12 -0400

    Companions reflect player projectiles

--- Assets/Scripts/standardBullet.cs
+++ Assets/Scripts/standardBullet.cs
@@ -6,6 +6,19 @@ using UnityEngine;
 public class standardBullet : MonoBehaviour
 {
 	public int damage = 0;
+	
+	private float lifeI = 0;
+	public float lifeEnd = 0;
+
+	private void Update()
+	{
+		lifeI += Time.deltaTime;
+
+		if (lifeI >= lifeEnd)
+		{
+			Destroy(this.gameObject);
+		}
+	}
 
 	private void FixedUpdate()
 	{

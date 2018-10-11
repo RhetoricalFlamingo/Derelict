26beb36ed487df13f994d43f6299a4ec9d46d2b9
MC <mcmailbox014@gmail.com>
2018-10-11 15:06:43 -0400

    Simple Chaser Enemies

--- Assets/Scripts/BattleScripts/gruntAI.cs
+++ Assets/Scripts/BattleScripts/gruntAI.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class gruntAI : MonoBehaviour
 {
 
+    public char enemyID = ' ';
+
 	public int maxHP = 0;
 	public int currentHP = 0;
+	private float moveSpeed = 20f;
+
+	public GameObject player;
+	public GameObject turnManager;
+	public bool realTime = false;
 	
 	// Use this for initialization
 	void Awake ()
@@ -15,10 +22,18 @@ public class gruntAI : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		realTime = turnManager.GetComponent<turnManagerScript>().realTime;
+		
 		if (currentHP <= 0)
 		{
 			Destroy(this.gameObject);
+		}
+
+		if (realTime && enemyID == 'c')
+		{
+			chaser();
 		}
 	}
 
@@ -26,5 +41,20 @@ public class gruntAI : MonoBehaviour
 	{
 		currentHP -= other.GetComponent<standardBullet>().damage;
 		Destroy(other.gameObject);
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		if (enemyID == 'c' && other.gameObject.tag == "Player")
+		{
+			Destroy(other.gameObject);
+			Debug.Log("playerDead");
+		}
+	}
+
+	public void chaser()
+	{
+		this.transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+		//Debug.Log("chasePlayer");
 	}
 }

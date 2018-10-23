3c3f876f29c6b1d5de26da693cc29ae9127fad59
MC <mcmailbox014@gmail.com>
2018-10-23 17:45:44 -0400

    Fixed screen border, enemies can now enter screen but players cant leave

--- Assets/Scripts/Coop Junk/MovePlayer.cs
+++ Assets/Scripts/Coop Junk/MovePlayer.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
+	private Vector2 LJOY_Write = new Vector2(0, 0);
+	private Vector2 RJOY_Write = new Vector2(0, 0);
+	
 	public float[] moveImpulse = new float[2];
 		public float dashSpeed = 0;
 	private bool[] dashing_dash = new bool[2];
@@ -15,6 +19,10 @@ public class MovePlayer : MonoBehaviour
 	
 	public GameObject[] chars = new GameObject[2];
 	public Rigidbody2D[] playerRBs = new Rigidbody2D[2];
+
+	public GameObject mainCam;
+	private float xBuffer = 171.5f;
+	private float yBuffer = 95f;
 	
 	// Use this for initialization
 	void Start () {
@@ -27,6 +35,9 @@ public class MovePlayer : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+		LJOY_Write = new Vector2(Input.GetAxis("LHorizontal"), Input.GetAxis("LVertical"));
+		RJOY_Write = new Vector2(Input.GetAxis("RHorizontal"), Input.GetAxis("RVertical"));
+		
 		//Swap();
 		Move(0);
 		Move(1);
@@ -36,15 +47,35 @@ public class MovePlayer : MonoBehaviour
 
 	void Move(int char_Index)	//MOVE playerCharacters
 	{
+		playerRBs[char_Index].velocity = new Vector2(0, 0);
+		
 		if (char_Index == 0)
 		{
-			playerRBs[char_Index].velocity = (new Vector2 ((moveImpulse[char_Index] * Input.GetAxis("LHorizontal")),
-				(moveImpulse[char_Index] * Input.GetAxis("LVertical"))));
+			if (chars[char_Index].transform.position.x < mainCam.transform.position.x - xBuffer && LJOY_Write.x < 0)	//Player0 camera boundaries, keeps player from moving away from camera once at screen edge
+				LJOY_Write.x = 0;
+			if (chars[char_Index].transform.position.x > mainCam.transform.position.x + xBuffer && LJOY_Write.x > 0)
+				LJOY_Write.x = 0;
+			if (chars[char_Index].transform.position.y < mainCam.transform.position.y - yBuffer && LJOY_Write.y < 0)
+				LJOY_Write.y = 0;
+			if (chars[char_Index].transform.position.y > mainCam.transform.position.y + yBuffer && LJOY_Write.y > 0)
+				LJOY_Write.y = 0;
+			
+			
+			playerRBs[char_Index].velocity += LJOY_Write * moveImpulse[char_Index];	//move player0
 		}
-		else
+		else if (char_Index == 1)
 		{
-			playerRBs[char_Index].velocity = (new Vector2 ((moveImpulse[char_Index] * Input.GetAxis("RHorizontal")),
-				(moveImpulse[char_Index] * Input.GetAxis("RVertical"))));
+			if (chars[char_Index].transform.position.x < mainCam.transform.position.x - xBuffer && RJOY_Write.x < 0)	//Player1 camera boundaries, keeps player from moving away from camera once at screen edge
+				RJOY_Write.x = 0;
+			if (chars[char_Index].transform.position.x > mainCam.transform.position.x + xBuffer && RJOY_Write.x > 0)
+				RJOY_Write.x = 0;
+			if (chars[char_Index].transform.position.y < mainCam.transform.position.y - yBuffer && RJOY_Write.y < 0)
+				RJOY_Write.y = 0;
+			if (chars[char_Index].transform.position.y > mainCam.transform.position.y + yBuffer && RJOY_Write.y > 0)
+				RJOY_Write.y = 0;
+
+
+			playerRBs[char_Index].velocity += RJOY_Write * moveImpulse[char_Index];	//move player1
 		}
 		
 		if (dashing_dash[char_Index])
@@ -53,45 +84,45 @@ public class MovePlayer : MonoBehaviour
 		}
 	}
 
-	void Dash(int dash_Index)		//DASH playerCharacters
+	void Dash(int char_Index)		//DASH playerCharacters
 	{
-		if (!dashing_dash[dash_Index] && !cooling_dash[dash_Index])	//transition from normal speed to dash
+		if (!dashing_dash[char_Index] && !cooling_dash[char_Index])	//transition from normal speed to dash
 		{
-			if (dash_Index == 0 && Input.GetButtonDown("L1"))
+			if (char_Index == 0 && Input.GetButtonDown("L1"))
 			{
 				moveImpulse[0] += dashSpeed;
 				dashing_dash[0] = true;
 			}
-			if (dash_Index == 1 && Input.GetButtonDown("R1"))
+			if (char_Index == 1 && Input.GetButtonDown("R1"))
 			{
 				moveImpulse[1] += dashSpeed;
 				dashing_dash[1] = true;
 			}
 		}
 
-		if (dashing_dash[dash_Index])	//transition from dash to cooldown
+		if (dashing_dash[char_Index])	//transition from dash to cooldown
 		{
-			dashDelayI[dash_Index] += Time.deltaTime;
+			dashDelayI[char_Index] += Time.deltaTime;
 
-			if (dashDelayI[dash_Index] > .3f)
+			if (dashDelayI[char_Index] > .3f)
 			{
-				moveImpulse[dash_Index] -= dashSpeed;
-				dashDelayI[dash_Index] = 0;
+				moveImpulse[char_Index] -= dashSpeed;
+				dashDelayI[char_Index] = 0;
 
-				dashing_dash[dash_Index] = false;
-				cooling_dash[dash_Index] = true;
+				dashing_dash[char_Index] = false;
+				cooling_dash[char_Index] = true;
 			}
 		}
 		
-		if (cooling_dash[dash_Index])	//transition from dash to cooldown
+		if (cooling_dash[char_Index])	//transition from dash to cooldown
 		{
-			dashDelayI[dash_Index] += Time.deltaTime;
+			dashDelayI[char_Index] += Time.deltaTime;
 
-			if (dashDelayI[dash_Index] > .45f)
+			if (dashDelayI[char_Index] > .45f)
 			{
-				dashDelayI[dash_Index] = 0;
+				dashDelayI[char_Index] = 0;
 
-				cooling_dash[dash_Index] = false;
+				cooling_dash[char_Index] = false;
 			}
 		}
 	}
